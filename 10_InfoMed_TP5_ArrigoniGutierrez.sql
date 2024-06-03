f124827ece91f0a71d853b55e24f7ea6e164cb06
estebanjgutierrez <106710633+estebanjgutierrez@users.noreply.github.com>
2024-06-03 20:46:40 -0300

    Revert "Students/gutierrez arrigoni/tp5 8 9 10"

--- 10_InfoMed_TP5_ArrigoniGutierrez.sql
+++
@@ -1,8 +0,0 @@
-SELECT M.nombre AS Nombre_Medicamento, COUNT(R.id_receta) AS Total_Recetas, 
-       T.nombre AS Nombre_Medico, P.nombre AS Nombre_Paciente
-FROM recetas R
-JOIN medicamentos M ON R.id_medicamento = M.id_medicamento
-JOIN medicos T ON R.id_medico = T.id_medico
-JOIN pacientes P ON R.ID_Paciente = P.ID_Paciente
-GROUP BY M.nombre, T.nombre, P.nombre
-ORDER BY Total_Recetas DESC;
